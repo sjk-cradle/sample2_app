b1e90030fd3a1b850d668ebdf8d230424d3f70fc
Ubuntu <ubuntu@ip-172-31-44-177.ap-northeast-1.compute.internal>
2025-11-08 08:51:42 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params) # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list.save  # 3. データをデータベースに保存するためのsaveメソッド実行
+    redirect_to '/top' # 4. トップ画面へリダイレクト
   end
 
   def index
@@ -9,5 +16,10 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
